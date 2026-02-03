3d6e1ce40f0a70651267e0b76eaacdec3390b702
howardsource <howard@sourcecreative.co.uk>
2026-02-03 15:00:48 +0000

    Add custom 20storieshigh theme and ACF plugin

---
+++ wp-content/themes/20storieshigh/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php bloginfo('name'); ?> - Coming Soon</title>
+    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="all" />
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+    <div class="container">
+        <h1>20 Stories High</h1>
+        <p>Our new website is currently under construction. Please check back soon.</p>
+    </div>
+    <?php wp_footer(); ?>
+</body>
+</html>
