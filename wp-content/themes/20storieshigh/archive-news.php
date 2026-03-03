9730246291aeab501c2a11e5110f8e361cbff156
howardsource <howard@sourcecreative.co.uk>
2026-03-03 12:10:09 +0000

    Update News Archive banner styles to match Projects Archive

--- wp-content/themes/20storieshigh/archive-news.php
+++ wp-content/themes/20storieshigh/archive-news.php
@@ -8,9 +8,17 @@ get_header();
 
 <div id="title-banner" class="outer no-image">
 	<div class="inner">
-		<h2 id="news-archive-title"><span><?php post_type_archive_title(); ?></span></h2>
+		<h2 id="projects-archive-title"><span><?php post_type_archive_title(); ?></span></h2>
 	</div>
 </div>
+
+<?php if (function_exists('yoast_breadcrumb')) : ?>
+<div id="breadcrumb-bar" class="outer navy">
+	<div class="inner breadcrumbs">
+		<?php yoast_breadcrumb(); ?>
+	</div>
+</div>
+<?php endif; ?>
 
 
     <div id="news-archive-content" class="outer module tiles">
@@ -22,7 +30,9 @@ get_header();
                             <div class="image" style="background-image: url('<?= get_field('thumbnail')['sizes']['tiles']; ?>')"></div>
                         </div>
                         <h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
-                        <p><?= get_the_excerpt(); ?></p>
+                        <h5><?= get_the_date(); ?></h5>
+                        <p><?= get_field('excerpt'); ?></p>
+                        <p class="large-button-link"><a href="<?php the_permalink(); ?>"><?php _e('Read more', '20storieshigh'); ?></a></p>
                     </div>
                 <?php endwhile; ?>
             <?php else : ?>
