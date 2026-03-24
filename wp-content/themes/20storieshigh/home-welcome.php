d97c6bbd8247b94839b691e7fad4e299bc3c4ff2
howardsource <howard@sourcecreative.co.uk>
2026-03-24 15:36:52 +0000

    Update home welcome

--- wp-content/themes/20storieshigh/home-welcome.php
+++ wp-content/themes/20storieshigh/home-welcome.php
@@ -3,8 +3,7 @@
         <div class="home-welcome-layout">
             <div class="home-welcome-left reveal"></div>
             <div class="home-welcome-text">
-                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.</p>
-                <p>clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
+                <?=  get_field('welcome_text'); ?>
             </div>
         </div>
     </div>
