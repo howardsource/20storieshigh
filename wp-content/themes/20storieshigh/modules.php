469c066cd3b5bfe3687d8f90bc5f7c23776f7d2d
howardsource <howard@sourcecreative.co.uk>
2026-03-25 13:40:52 +0000

    Harden modules loader + full width image tweaks

--- wp-content/themes/20storieshigh/modules.php
+++ wp-content/themes/20storieshigh/modules.php
@@ -5,9 +5,26 @@
 	$carouselN = 1;
 	if($modules!=false) :
 	foreach($modules as $module):
-		include(locate_template('modules/'.$module['acf_fc_layout'].'.php')); 
+		$layout = $module['acf_fc_layout'] ?? '';
+		$template_rel = $layout ? 'modules/' . $layout . '.php' : '';
+		$template_path = $template_rel ? locate_template($template_rel) : '';
+		if ($template_path) {
+			include $template_path;
+		} else {
+			$missing_label = $template_rel ?: 'modules/(missing acf_fc_layout).php';
+			error_log('Missing module template: ' . $missing_label);
+			if (current_user_can('manage_options')) :
+				?>
+				<div class="outer bricklight">
+					<div class="inner">
+						<p>Missing module template: <?= esc_html($missing_label); ?></p>
+					</div>
+				</div>
+				<?php
+			endif;
+		}
 		$modN++;
-		if($module['acf_fc_layout']=='carousel' || $module['acf_fc_layout']=='panel_slider'|| $module['acf_fc_layout']=='latest_news') :
+		if($layout=='carousel' || $layout=='panel_slider'|| $layout=='latest_news') :
 			$carouselN++;
 		endif;
 	endforeach; 
--- wp-content/themes/20storieshigh/modules/full_width_image.php
+++ wp-content/themes/20storieshigh/modules/full_width_image.php
@@ -1,14 +1,32 @@
 <?php 
 $class = 'image';
-if($module['width']=='column') : 
+$width = $module['width'] ?? '';
+if ($width === 'column') :
 	$class .= ' inner';
 endif;	
-if($module['fixed_position']==true) :
+$fixed_position = !empty($module['fixed_position']);
+if ($fixed_position) :
 	$class .= ' fixed-scroll';
 endif;
+
+$background_colour = $module['background_colour'] ?? 'white';
+$image = $module['image'] ?? null;
+$image_url = '';
+$image_alt = '';
+$image_width = '';
+$image_height = '';
+
+if (is_array($image)) {
+	$image_url = $image['sizes']['carousel'] ?? ($image['url'] ?? '');
+	$image_alt = $image['alt'] ?? '';
+	$image_width = $image['sizes']['carousel-width'] ?? '';
+	$image_height = $image['sizes']['carousel-height'] ?? '';
+}
 ?>
-<div class="module full-width-image outer">
-	<div class="<?= $class; ?>" style="background-image: url('<?php echo $module['image']['sizes']['carousel']; ?>')">
-		<img src="<?php echo $module['image']['sizes']['carousel']; ?>" width="<?php echo $module['image']['sizes']['carousel-width']; ?>" height="<?php echo $module['image']['sizes']['carousel-height']; ?>" alt="<?php echo $module['image']['alt']; ?>" />
+<div class="module full-width-image outer <?= esc_attr($background_colour); ?>">
+	<div class="<?= esc_attr($class); ?>"<?php if ($image_url) : ?> style="background-image: url('<?= esc_url($image_url); ?>')"<?php endif; ?>>
+		<?php if ($image_url) : ?>
+		<img src="<?= esc_url($image_url); ?>"<?php if ($image_width) : ?> width="<?= esc_attr($image_width); ?>"<?php endif; ?><?php if ($image_height) : ?> height="<?= esc_attr($image_height); ?>"<?php endif; ?> alt="<?= esc_attr($image_alt); ?>" />
+		<?php endif; ?>
 	</div>
 </div>
