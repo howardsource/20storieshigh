b5b244c90746e7ec5b9c5c51c5654c93b5df0a1c
howardsource <howard@sourcecreative.co.uk>
2026-06-17 10:00:02 +0100

    Add resource short display title override

--- wp-content/themes/20storieshigh/modules/resources.php
+++ wp-content/themes/20storieshigh/modules/resources.php
@@ -1,11 +1,11 @@
 <article class="module resources outer <?=  $module['background_colour']; ?>">
     <div class="inner">
-        <h3><?= get_field('resources_title') ?: 'Resources'; ?></h3>
+        <h3><?= $module['resources_title'] ?: 'Resources'; ?></h3>
         <div class="resource-group">
         <?php foreach($module['resources'] as $resource) : ?>
         <div class="resource">
             <?php $download = get_field('download', $resource->ID); ?>
-            <h4><?= $resource->post_title; ?></h4>
+            <h4><?= !empty(get_field('short_display_title', $resource->ID)) ? get_field('short_display_title', $resource->ID) : $resource->post_title; ?></h4>
             <p><?= get_field('overview', $resource->ID); ?></p>
             <?php if (!empty($download['url'])) : ?>
             <p class="large-button-link"><a href="<?= $download['url']; ?>">Download (PDF)</a></p>
